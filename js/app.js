37fc5a91adee2c792445a6713a5b5ef1598e2eef
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-01-26 15:55:13 +0000

    Color spawning

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const colorsContainers = document.querySelector('.colors-container')
+const spawnButton = document.querySelector('#spawn-button')
+
+let colorBlocks = ['', '', '', '']
+
+// to get a random number for the color generation
+function getRandomNumber()
+{
+    return Math.floor(Math.random() * 255)
+}
+
+// to generate the colors
+function spawn()
+{
+    // to clear the previous colors
+    colorsContainers.innerHTML = ''
+
+    const commonColor = getRandomNumber()
+
+    colorBlocks.forEach((_colorBlock, index) => {
+        // to display the colors text
+        colorBlocks[index] =
+            'rgb('
+        + commonColor +
+        ',' +
+        getRandomNumber() +
+        ',' +
+        getRandomNumber() +
+        ')'
+    })
+
+    // to display the colors
+    colorBlocks.forEach(colorBlock =>
+    {
+        const divElement = document.createElement('div')
+        divElement.style.backgroundColor = colorBlock
+        divElement.textContent = colorBlock
+        colorsContainers.append(divElement)
+    })
+
+}
+
+spawn()
+spawnButton.addEventListener('click', spawn)
+document.addEventListener('keydown', spawn)
